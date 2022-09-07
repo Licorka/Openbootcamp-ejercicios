1218f4bec34ba7657201e3e4efafef89193325f1
Licorka <113046540+Licorka@users.noreply.github.com>
2022-09-07 18:22:07 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(6,7,8);
+    }
+
+    public static void suma(int a, int b, int c){
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+
+        Coche nuevoCoche = new Coche();
+
+        nuevoCoche.anadirPuerta();
+        nuevoCoche.anadirPuerta();
+
+        System.out.println("Este coche tiene: " + nuevoCoche.puertas + " puertas");
+
+    }
+
+    static class Coche {
+        public int puertas = 0;
+
+        public void anadirPuerta() {
+                this.puertas++;
+            }
+    }
+}
