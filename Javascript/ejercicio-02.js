7609cd0b2441c2dc37d3b719b341cbd5e9197472
Licorka <113046540+Licorka@users.noreply.github.com>
2022-10-07 10:51:51 +0200

    Create ejercicio-02.js

---
+++ Javascript/ejercicio-02.js
@@ -0,0 +1,11 @@
+const lista = [
+  "Alex",
+  38,
+  true,
+  new Date(1984, 05, 24),
+  { 
+      titulo: "Sandman",
+      autor: "Neil Gainman",
+      fecha: new Date(2020, 0, 1),
+      url: "hhttps://www.amazon.es/Biblioteca-Sandman-Vol-Preludios-C/"
+    },
